94f0bb3d3ffe78ead6e6404d52bc40872cdeae3a
Lismey Plasencia <lismeyplasencia@gmail.com>
2021-01-22 16:24:07 -0500

    update js file

--- script.js
+++ script.js
@@ -2,26 +2,72 @@
 
 console.log(dayjs())
 
+// Declare variables
+var saveBtn = $("button").addClass("saveBtn")
+
+
+
 $( document ).ready(function(){
 
     // Grab the current date from Dayjs
-    
-    var currentDay = dayjs().format('dddd MMMM DD');
+
+    var currentDay = dayjs().format('dddd MMMM DD, YYYY');
     console.log(currentDay);
 
     // Display the current date on the HTML page
 
     $("#currentDay").text(currentDay)
     
-
-})
-
+    // Color code the time blocks to indicate whether it is in the past, present or future
 
 
-//WHEN I scroll down
-//THEN I am presented with time blocks for standard business hours
+    var currentHour = parseInt(dayjs().hour());
+    console.log(currentHour);
+
+    
+    // Create an Array to enter the data attributes of the timeblocks    
+    
+    // for loop to check IF it's present, past or future and change the class taking into consideration the status
+
+
+
+    // create an event listener to the saveButton to save in local storage the saved task
+
+    //Check when i refresh the page the tasks remains displayed
+
+
+    
+
+
+
+
+   
+
+    
+
+
+    
+
+    
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+ 
 //WHEN I view the time blocks for that day
 //THEN each time block is color-coded to indicate whether it is in the past, present, or future
+
+
 //WHEN I click into a time block
 //THEN I can enter an event
 //WHEN I click the save button for that time block
@@ -29,3 +75,12 @@ $( document ).ready(function(){
 //WHEN I refresh the page
 //THEN the saved events persist
 //```
+
+
+
+
+})
+
+
+
+
