7ce2711da1112e25fd606d301c2a6579f1830be7
Lismey Plasencia <lismeyplasencia@gmail.com>
2021-01-23 17:38:46 -0500

    updated js file

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # daily-schedule
+
+Ok so here is what I got, I moved the data-number attr into the div tag with the class of time, you can see below
+<!-- 9AM Timeblock  -->
+    <div class="row">
+      <div class="col-sm-2" class="">
+        <div id="time1" class="col-2 border text-center time" data-number="9">9AM</div>
+      </div>
+        <div class="col-sm-8">
+          <textarea class="textarea" ></textarea>
+        </div>
+        <div class="col-sm-2">
+        <button class="btn" type="button">Save </button>
+      </div>
+    </div>
+10:18
+then using jquery you can do this
+$('.time').each(function(){
+      var blockHour = parseInt($(this).attr('data-number'));
+    })
+10:19
+loop through all of the tags that have the .time attr and then save the data-number as a variable and convert it into an interger
--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-
-
 console.log(dayjs())
-
-// Declare variables
 var saveBtn = $("button").addClass("saveBtn")
-
-
 
 $( document ).ready(function(){
 
@@ -18,17 +12,191 @@ $( document ).ready(function(){
 
     $("#currentDay").text(currentDay)
     
-    // Color code the time blocks to indicate whether it is in the past, present or future
+     // create an event listener to the saveButton to save in local storage the saved task
 
+    saveBtn.on("click", function(){
+        var textInput = $("textarea").text();
+        console.log(textInput);
+        
+
+
+
+
+
+
+
+       
+
+
+
+
+    })
+
+    // Color code the time blocks to indicate whether it is in the past, present or future
 
     var currentHour = parseInt(dayjs().hour());
     console.log(currentHour);
+    
+    
+
+
+    var time9 = $("#time1").data("number")
+    console.log(time9)
+    
+
+     if (time9 < currentHour) {
+               $("#textArea1").addClass("past");    
+                
+            }
+    
+            else if (time9 > currentHour) {
+                $("#textArea1").addClass("future"); 
+                
+            }
+            else {
+                $("#textArea1").addClass("present"); 
+            }
+
+
+    var time10 = $("#time2").data("number")
+    console.log(time10)
+
+    if (time10 < currentHour) {
+        $("#textArea2").addClass("past");    
+         
+     }
+
+     else if (time10 > currentHour) {
+         $("#textArea2").addClass("future"); 
+         
+     }
+     else {
+         $("#textArea2").addClass("present"); 
+     }
+
+
+
+    var time11 = $("#time3").data("number")
+    console.log(time11)
+
+    if (time11 < currentHour) {
+        $("#textArea3").addClass("past");    
+         
+     }
+
+     else if (time11 > currentHour) {
+         $("#textArea3").addClass("future"); 
+         
+     }
+     else {
+         $("#textArea3").addClass("present"); 
+     }
+
+    var time12 = $("#time4").data("number")
+    console.log(time12)
+
+    if (time12 < currentHour) {
+        $("#textArea4").addClass("past");    
+         
+     }
+
+     else if (time12 > currentHour) {
+         $("#textArea4").addClass("future"); 
+         
+     }
+     else {
+         $("#textArea4").addClass("present"); 
+     }
+
+    var time13 = $("#time5").data("number")
+    console.log(time13)
+
+    if (time13 < currentHour) {
+        $("#textArea5").addClass("past");    
+         
+     }
+
+     else if (time13 > currentHour) {
+         $("#textArea5").addClass("future"); 
+         
+     }
+     else {
+         $("#textArea13").addClass("present"); 
+     }
+
+    var time14 = $("#time6").data("number")
+    console.log(time14)
+
+    if (time14 < currentHour) {
+        $("#textArea6").addClass("past");    
+         
+     }
+
+     else if (time14 > currentHour) {
+         $("#textArea6").addClass("future"); 
+         
+     }
+     else {
+         $("#textArea6").addClass("present"); 
+     }
+
+    var time15 = $("#time7").data("number")
+    console.log(time15)
+
+    if (time15 < currentHour) {
+        $("#textArea7").addClass("past");    
+         
+     }
+
+     else if (time15 > currentHour) {
+         $("#textArea7").addClass("future"); 
+         
+     }
+     else {
+         $("#textArea7").addClass("present"); 
+     }
+
+    var time16 = $("#time8").data("number")
+    console.log(time16)
+
+    if (time16 < currentHour) {
+        $("#textArea8").addClass("past");    
+         
+     }
+
+     else if (time16 > currentHour) {
+         $("#textArea8").addClass("future"); 
+         
+     }
+     else {
+         $("#textArea8").addClass("present"); 
+     }
+
+    var time17 = $("#time9").data("number")
+    console.log(time17)
+
+    if (time17 < currentHour) {
+        $("#textArea9").addClass("past");    
+         
+     }
+
+     else if (time17 > currentHour) {
+         $("#textArea9").addClass("future"); 
+         
+     }
+     else {
+         $("#textArea9").addClass("present"); 
+     }
+
 
     
-    // Create an Array to enter the data attributes of the timeblocks    
     
-    // for loop to check IF it's present, past or future and change the class taking into consideration the status
+      
 
+      })
+   
+      
+    
 
 
     // create an event listener to the saveButton to save in local storage the saved task
@@ -79,7 +247,7 @@ $( document ).ready(function(){
 
 
 
-})
+//})
 
 
 
